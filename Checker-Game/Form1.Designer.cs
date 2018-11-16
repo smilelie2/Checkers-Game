d6d87f043cb9b50899fca2e60ce0a33dc34db893
Zan <piyachok.d@ku.th>
2018-11-16 12:04:14 +0700

    Add White and can simple move

--- Checker-Game/Form1.Designer.cs
+++ Checker-Game/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Checker_Game
 {
-    partial class Form1
+    partial class Form
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,183 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form));
+            this.CheckerBoard = new System.Windows.Forms.PictureBox();
+            this.p1_1 = new System.Windows.Forms.PictureBox();
+            this.p1_2 = new System.Windows.Forms.PictureBox();
+            this.p1_3 = new System.Windows.Forms.PictureBox();
+            this.p1_4 = new System.Windows.Forms.PictureBox();
+            this.p1_5 = new System.Windows.Forms.PictureBox();
+            this.p1_6 = new System.Windows.Forms.PictureBox();
+            this.p1_7 = new System.Windows.Forms.PictureBox();
+            this.p1_8 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.CheckerBoard)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1_3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1_4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1_5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1_6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1_7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1_8)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CheckerBoard
+            // 
+            this.CheckerBoard.Image = ((System.Drawing.Image)(resources.GetObject("CheckerBoard.Image")));
+            this.CheckerBoard.Location = new System.Drawing.Point(-1, -1);
+            this.CheckerBoard.Name = "CheckerBoard";
+            this.CheckerBoard.Size = new System.Drawing.Size(720, 720);
+            this.CheckerBoard.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.CheckerBoard.TabIndex = 0;
+            this.CheckerBoard.TabStop = false;
+            // 
+            // p1_1
+            // 
+            this.p1_1.BackColor = System.Drawing.Color.Transparent;
+            this.p1_1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.p1_1.Image = ((System.Drawing.Image)(resources.GetObject("p1_1.Image")));
+            this.p1_1.InitialImage = null;
+            this.p1_1.Location = new System.Drawing.Point(-1, 537);
+            this.p1_1.Name = "p1_1";
+            this.p1_1.Size = new System.Drawing.Size(92, 92);
+            this.p1_1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p1_1.TabIndex = 1;
+            this.p1_1.TabStop = false;
+            this.p1_1.Click += new System.EventHandler(this.p1_1_Click);
+            // 
+            // p1_2
+            // 
+            this.p1_2.BackColor = System.Drawing.Color.Transparent;
+            this.p1_2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.p1_2.Image = ((System.Drawing.Image)(resources.GetObject("p1_2.Image")));
+            this.p1_2.InitialImage = null;
+            this.p1_2.Location = new System.Drawing.Point(87, 627);
+            this.p1_2.Name = "p1_2";
+            this.p1_2.Size = new System.Drawing.Size(92, 92);
+            this.p1_2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p1_2.TabIndex = 2;
+            this.p1_2.TabStop = false;
+            // 
+            // p1_3
+            // 
+            this.p1_3.BackColor = System.Drawing.Color.Transparent;
+            this.p1_3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.p1_3.Image = ((System.Drawing.Image)(resources.GetObject("p1_3.Image")));
+            this.p1_3.InitialImage = null;
+            this.p1_3.Location = new System.Drawing.Point(177, 537);
+            this.p1_3.Name = "p1_3";
+            this.p1_3.Size = new System.Drawing.Size(92, 92);
+            this.p1_3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p1_3.TabIndex = 2;
+            this.p1_3.TabStop = false;
+            // 
+            // p1_4
+            // 
+            this.p1_4.BackColor = System.Drawing.Color.Transparent;
+            this.p1_4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.p1_4.Image = ((System.Drawing.Image)(resources.GetObject("p1_4.Image")));
+            this.p1_4.InitialImage = null;
+            this.p1_4.Location = new System.Drawing.Point(269, 627);
+            this.p1_4.Name = "p1_4";
+            this.p1_4.Size = new System.Drawing.Size(92, 92);
+            this.p1_4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p1_4.TabIndex = 2;
+            this.p1_4.TabStop = false;
+            // 
+            // p1_5
+            // 
+            this.p1_5.BackColor = System.Drawing.Color.Transparent;
+            this.p1_5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.p1_5.Image = ((System.Drawing.Image)(resources.GetObject("p1_5.Image")));
+            this.p1_5.InitialImage = null;
+            this.p1_5.Location = new System.Drawing.Point(357, 537);
+            this.p1_5.Name = "p1_5";
+            this.p1_5.Size = new System.Drawing.Size(92, 92);
+            this.p1_5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p1_5.TabIndex = 2;
+            this.p1_5.TabStop = false;
+            // 
+            // p1_6
+            // 
+            this.p1_6.BackColor = System.Drawing.Color.Transparent;
+            this.p1_6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.p1_6.Image = ((System.Drawing.Image)(resources.GetObject("p1_6.Image")));
+            this.p1_6.InitialImage = null;
+            this.p1_6.Location = new System.Drawing.Point(447, 627);
+            this.p1_6.Name = "p1_6";
+            this.p1_6.Size = new System.Drawing.Size(92, 92);
+            this.p1_6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p1_6.TabIndex = 2;
+            this.p1_6.TabStop = false;
+            // 
+            // p1_7
+            // 
+            this.p1_7.BackColor = System.Drawing.Color.Transparent;
+            this.p1_7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.p1_7.Image = ((System.Drawing.Image)(resources.GetObject("p1_7.Image")));
+            this.p1_7.InitialImage = null;
+            this.p1_7.Location = new System.Drawing.Point(538, 537);
+            this.p1_7.Name = "p1_7";
+            this.p1_7.Size = new System.Drawing.Size(92, 92);
+            this.p1_7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p1_7.TabIndex = 2;
+            this.p1_7.TabStop = false;
+            // 
+            // p1_8
+            // 
+            this.p1_8.BackColor = System.Drawing.Color.Transparent;
+            this.p1_8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.p1_8.Image = ((System.Drawing.Image)(resources.GetObject("p1_8.Image")));
+            this.p1_8.InitialImage = null;
+            this.p1_8.Location = new System.Drawing.Point(627, 627);
+            this.p1_8.Name = "p1_8";
+            this.p1_8.Size = new System.Drawing.Size(92, 92);
+            this.p1_8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p1_8.TabIndex = 2;
+            this.p1_8.TabStop = false;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(718, 719);
+            this.Controls.Add(this.p1_8);
+            this.Controls.Add(this.p1_7);
+            this.Controls.Add(this.p1_6);
+            this.Controls.Add(this.p1_5);
+            this.Controls.Add(this.p1_4);
+            this.Controls.Add(this.p1_3);
+            this.Controls.Add(this.p1_2);
+            this.Controls.Add(this.p1_1);
+            this.Controls.Add(this.CheckerBoard);
+            this.MaximizeBox = false;
+            this.Name = "Form";
+            this.Text = "Checkers";
+            ((System.ComponentModel.ISupportInitialize)(this.CheckerBoard)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1_3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1_4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1_5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1_6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1_7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1_8)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox CheckerBoard;
+        private System.Windows.Forms.PictureBox p1_1;
+        private System.Windows.Forms.PictureBox p1_2;
+        private System.Windows.Forms.PictureBox p1_3;
+        private System.Windows.Forms.PictureBox p1_4;
+        private System.Windows.Forms.PictureBox p1_5;
+        private System.Windows.Forms.PictureBox p1_6;
+        private System.Windows.Forms.PictureBox p1_7;
+        private System.Windows.Forms.PictureBox p1_8;
     }
 }
 
--- Checker-Game/Form1.cs
+++ Checker-Game/Form1.cs
@@ -10,11 +10,35 @@ using System.Windows.Forms;
 
 namespace Checker_Game
 {
-    public partial class Form1 : Form
+    public partial class Form : System.Windows.Forms.Form
     {
-        public Form1()
+        private char[,] board = new char[8, 8];
+        public Form()
         {
             InitializeComponent();
+            CheckerBoard.Controls.Add(p1_1);
+            CheckerBoard.Controls.Add(p1_2);
+            CheckerBoard.Controls.Add(p1_3);
+            CheckerBoard.Controls.Add(p1_4);
+            CheckerBoard.Controls.Add(p1_5);
+            CheckerBoard.Controls.Add(p1_6);
+            CheckerBoard.Controls.Add(p1_7);
+            CheckerBoard.Controls.Add(p1_8);
+
+            board[0, 6] = 'R';
+            board[1, 7] = 'R';
+            board[2, 6] = 'R';
+            board[3, 7] = 'R';
+            board[4, 6] = 'R';
+            board[5, 7] = 'R';
+            board[6, 6] = 'R';
+            board[7, 7] = 'R';
+        }
+
+        private void p1_1_Click(object sender, EventArgs e)
+        {
+            p1_1.Left += 90;
+            p1_1.Top -= 90;
         }
     }
 }
